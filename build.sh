13316387cde1e7daa62320fd09c90befed46dae2
Andy Li <andy@onthewings.net>
2016-08-21 18:18:09 +0800

    build.sh: added missing ref to Generated.Custom.cs, format commands

--- build.sh
+++ build.sh
@@ -7,6 +7,29 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-mcs /out:ClangSharpPInvokeGenerator.exe ClangSharpPInvokeGenerator/ClangSharp.Extensions.cs ClangSharpPInvokeGenerator/EnumVisitor.cs ClangSharpPInvokeGenerator/Extensions.cs ClangSharpPInvokeGenerator/ForwardDeclarationVisitor.cs ClangSharpPInvokeGenerator/FunctionVisitor.cs ClangSharpPInvokeGenerator/Generated.cs ClangSharpPInvokeGenerator/ICXCursorVisitor.cs ClangSharpPInvokeGenerator/Program.cs ClangSharpPInvokeGenerator/StructVisitor.cs ClangSharpPInvokeGenerator/TypeDefVisitor.cs
-mono ClangSharpPInvokeGenerator.exe --m clang --p clang_ --namespace ClangSharp --output Generated.cs --libraryPath $1 --include $2 --file $2/clang-c/Index.h --file $2/clang-c/CXString.h --file $2/clang-c/Documentation.h --file $2/clang-c/CXErrorCode.h
+mcs /out:ClangSharpPInvokeGenerator.exe \
+	ClangSharpPInvokeGenerator/ClangSharp.Extensions.cs \
+	ClangSharpPInvokeGenerator/EnumVisitor.cs \
+	ClangSharpPInvokeGenerator/Extensions.cs \
+	ClangSharpPInvokeGenerator/ForwardDeclarationVisitor.cs \
+	ClangSharpPInvokeGenerator/FunctionVisitor.cs \
+	ClangSharpPInvokeGenerator/Generated.cs \
+	ClangSharpPInvokeGenerator/Generated.Custom.cs \
+	ClangSharpPInvokeGenerator/ICXCursorVisitor.cs \
+	ClangSharpPInvokeGenerator/Program.cs \
+	ClangSharpPInvokeGenerator/StructVisitor.cs \
+	ClangSharpPInvokeGenerator/TypeDefVisitor.cs
+
+mono ClangSharpPInvokeGenerator.exe \
+	--m clang \
+	--p clang_ \
+	--namespace ClangSharp \
+	--output Generated.cs \
+	--libraryPath $1 \
+	--include $2 \
+	--file $2/clang-c/Index.h \
+	--file $2/clang-c/CXString.h \
+	--file $2/clang-c/Documentation.h \
+	--file $2/clang-c/CXErrorCode.h
+
 mcs /target:library /out:ClangSharp.dll Generated.cs Extensions.cs
