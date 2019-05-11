f2a2e50f3783864f97447844f3052a88c0bb284a
Tanner Gooding <tagoo@outlook.com>
2019-05-11 13:16:26 -0700

    Adding support for parsing expressions attached to EnumConstantDecl

---
+++ ClangSharpPInvokeGenerator/AttachedOperatorData.cs
@@ -0,0 +1,30 @@
+﻿using ClangSharp;
+using System.Diagnostics;
+
+namespace ClangSharpPInvokeGenerator
+{
+    public struct AttachedOperatorData
+    {
+        public AttachedOperatorData(CXCursorKind kind, string @operator)
+        {
+            Kind = kind;
+            Operator = @operator;
+
+            if (kind == CXCursorKind.CXCursor_UnaryOperator)
+            {
+                RemainingOperandCount = 1;
+            }
+            else
+            {
+                Debug.Assert(kind == CXCursorKind.CXCursor_BinaryOperator);
+                RemainingOperandCount = 2;
+            }
+        }
+
+        public CXCursorKind Kind { get; }
+
+        public string Operator { get; }
+
+        public int RemainingOperandCount { get; set; }
+    }
+}
