0d451c567ff496e797c5b1edf3ddd0c9bfdb6602
Tanner Gooding <tagoo@outlook.com>
2019-05-26 07:34:07 -0700

    Adding some basic StructDeclaration tests covering fields

---
+++ ClangSharp.PInvokeGenerator.Test/StructDeclarationTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace ClangSharp.Test
+{
+    public sealed class StructDeclarationTest : PInvokeGeneratorTest
+    {
+        [Theory]
+        [InlineData("unsigned char", "byte")]
+        [InlineData("double", "double")]
+        [InlineData("short", "short")]
+        [InlineData("int", "int")]
+        [InlineData("long long", "long")]
+        [InlineData("char", "sbyte")]
+        [InlineData("float", "float")]
+        [InlineData("unsigned short", "ushort")]
+        [InlineData("unsigned int", "uint")]
+        [InlineData("unsigned long long", "ulong")]
+        public async Task BasicTest(string nativeType, string expectedManagedType)
+        {
+            var inputContents = $@"struct MyStruct
+{{
+    {nativeType} r;
+    {nativeType} g;
+    {nativeType} b;
+}};
+";
+
+            var expectedOutputContents = $@"namespace ClangSharp.Test
+{{
+    public partial struct MyStruct
+    {{
+        public {expectedManagedType} r;
+        public {expectedManagedType} g;
+        public {expectedManagedType} b;
+    }}
+}}
+";
+
+            await ValidateGeneratedBindings(inputContents, expectedOutputContents);
+        }
+    }
+}
