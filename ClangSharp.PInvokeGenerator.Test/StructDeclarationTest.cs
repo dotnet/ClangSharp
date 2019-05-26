5b3ed8b10af7c54529c5fd919a1e7fb5942fc89c
Tanner Gooding <tagoo@outlook.com>
2019-05-26 07:52:45 -0700

    Adding a basic test covering use of a typedef

--- ClangSharp.PInvokeGenerator.Test/StructDeclarationTest.cs
+++ ClangSharp.PInvokeGenerator.Test/StructDeclarationTest.cs
@@ -39,5 +39,52 @@ namespace ClangSharp.Test
 
             await ValidateGeneratedBindings(inputContents, expectedOutputContents);
         }
+
+        [Theory]
+        [InlineData("unsigned char", "byte")]
+        [InlineData("double", "double")]
+        [InlineData("short", "short")]
+        [InlineData("int", "int")]
+        [InlineData("long long", "long")]
+        [InlineData("char", "sbyte")]
+        [InlineData("float", "float")]
+        [InlineData("unsigned short", "ushort")]
+        [InlineData("unsigned int", "uint")]
+        [InlineData("unsigned long long", "ulong")]
+        public async Task TypedefTest(string nativeType, string expectedManagedType)
+        {
+            var inputContents = $@"typedef {nativeType} MyTypedefAlias;
+
+struct MyStruct
+{{
+    MyTypedefAlias r;
+    MyTypedefAlias g;
+    MyTypedefAlias b;
+}};
+";
+
+            var expectedOutputContents = $@"namespace ClangSharp.Test
+{{
+    public partial struct MyTypedefAlias
+    {{
+        public MyTypedefAlias({expectedManagedType} value)
+        {{
+            Value = value;
+        }}
+
+        public {expectedManagedType} Value;
+    }}
+
+    public partial struct MyStruct
+    {{
+        public MyTypedefAlias r;
+        public MyTypedefAlias g;
+        public MyTypedefAlias b;
+    }}
+}}
+";
+
+            await ValidateGeneratedBindings(inputContents, expectedOutputContents);
+        }
     }
 }
