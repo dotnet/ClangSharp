f9d2cb31da8c9d1f2a238bfb7984000297e80bfb
Tanner Gooding <tagoo@outlook.com>
2019-05-26 07:33:41 -0700

    Adding some more EnumDeclaration tests covering explicit base types

--- ClangSharp.PInvokeGenerator.Test/EnumDeclarationTest.cs
+++ ClangSharp.PInvokeGenerator.Test/EnumDeclarationTest.cs
@@ -55,26 +55,33 @@ namespace ClangSharp.Test
             await ValidateGeneratedBindings(inputContents, expectedOutputContents);
         }
 
-        [Fact]
-        public async Task BasicTypedTest()
+        [Theory]
+        [InlineData("unsigned char", "byte")]
+        [InlineData("short", "short")]
+        [InlineData("long long", "long")]
+        [InlineData("char", "sbyte")]
+        [InlineData("unsigned short", "ushort")]
+        [InlineData("unsigned int", "uint")]
+        [InlineData("unsigned long long", "ulong")]
+        public async Task ExplicitTypedTest(string nativeType, string expectedManagedType)
         {
-            var inputContents = @"enum MyEnum : unsigned char
-{
+            var inputContents = $@"enum MyEnum : {nativeType}
+{{
     MyEnum_Value0,
     MyEnum_Value1,
     MyEnum_Value2,
-};
+}};
 ";
 
-            var expectedOutputContents = @"namespace ClangSharp.Test
-{
-    public enum MyEnum : byte
-    {
+            var expectedOutputContents = $@"namespace ClangSharp.Test
+{{
+    public enum MyEnum : {expectedManagedType}
+    {{
         MyEnum_Value0,
         MyEnum_Value1,
         MyEnum_Value2,
-    }
-}
+    }}
+}}
 ";
 
             await ValidateGeneratedBindings(inputContents, expectedOutputContents);
