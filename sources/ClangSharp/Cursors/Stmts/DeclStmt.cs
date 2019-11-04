8a02fe81f98f6cb3e4602470e8dbb8188469a921
udaken <uda.ken+git@gmail.com>
2019-11-04 16:48:30 +0900

    Adding DeclStmt.Decls

--- sources/ClangSharp/Cursors/Stmts/DeclStmt.cs
+++ sources/ClangSharp/Cursors/Stmts/DeclStmt.cs
@@ -1,11 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using ClangSharp.Interop;
 
 namespace ClangSharp
 {
     public sealed class DeclStmt : Stmt
     {
+        private readonly Lazy<IReadOnlyList<Decl>> _children;
         internal DeclStmt(CXCursor handle) : base(handle, CXCursorKind.CXCursor_DeclStmt)
         {
+            _children = new Lazy<IReadOnlyList<Decl>>(() => CursorChildren.OfType<Decl>().ToList());
         }
+
+        public IReadOnlyList<Decl> Decls => _children.Value;
     }
 }
