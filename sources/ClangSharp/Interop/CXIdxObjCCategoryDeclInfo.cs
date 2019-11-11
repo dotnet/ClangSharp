b1c20924b0658b55d823831b238b1b5924572998
Tanner Gooding <tagoo@outlook.com>
2019-11-10 19:20:07 -0800

    Adding a license header to all the code files

--- sources/ClangSharp/Interop/CXIdxObjCCategoryDeclInfo.cs
+++ sources/ClangSharp/Interop/CXIdxObjCCategoryDeclInfo.cs
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft and Contributors. All rights reserved. Licensed under the University of Illinois/NCSA Open Source License. See LICENSE.txt in the project root for license information.
+
 namespace ClangSharp.Interop
 {
     public unsafe partial struct CXIdxObjCCategoryDeclInfo
