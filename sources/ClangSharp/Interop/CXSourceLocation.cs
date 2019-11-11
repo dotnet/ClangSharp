c0712e279fd410178f4bda07ae6f01795c70e155
Tanner Gooding <tagoo@outlook.com>
2019-11-10 19:33:03 -0800

    Adding a "ported from" header to the auto-generated interop code

--- sources/ClangSharp/Interop/CXSourceLocation.cs
+++ sources/ClangSharp/Interop/CXSourceLocation.cs
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft and Contributors. All rights reserved. Licensed under the University of Illinois/NCSA Open Source License. See LICENSE.txt in the project root for license information.
 
+// Ported from https://github.com/llvm/llvm-project/tree/llvmorg-9.0.0/clang/include/clang-c
+// Original source is Copyright (c) the LLVM Project and Contributors. Licensed under the Apache License v2.0 with LLVM Exceptions. See NOTICE.txt in the project root for license information.
+
 namespace ClangSharp.Interop
 {
     public partial struct CXSourceLocation
