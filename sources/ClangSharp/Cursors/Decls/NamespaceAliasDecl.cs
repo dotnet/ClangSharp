8cabb3a1ba2269e931593afca97e127521013b6b
Tanner Gooding <tagoo@outlook.com>
2019-11-11 12:54:42 -0800

    Applying formatting fixes to NamespaceAliasDecl and UsingDirectiveDecl

--- sources/ClangSharp/Cursors/Decls/NamespaceAliasDecl.cs
+++ sources/ClangSharp/Cursors/Decls/NamespaceAliasDecl.cs
@@ -1,8 +1,10 @@
+// Copyright (c) Microsoft and Contributors. All rights reserved. Licensed under the University of Illinois/NCSA Open Source License. See LICENSE.txt in the project root for license information.
+
 using ClangSharp.Interop;
 
 namespace ClangSharp
 {
-    public sealed class NamespaceAliasDecl: NamedDecl, IRedeclarable<NamespaceDecl>
+    public sealed class NamespaceAliasDecl : NamedDecl, IRedeclarable<NamespaceDecl>
     {
         internal NamespaceAliasDecl(CXCursor handle) : base(handle, CXCursorKind.CXCursor_NamespaceAlias)
         {
--- sources/ClangSharp/Cursors/Decls/UsingDirectiveDecl.cs
+++ sources/ClangSharp/Cursors/Decls/UsingDirectiveDecl.cs
@@ -1,10 +1,10 @@
+// Copyright (c) Microsoft and Contributors. All rights reserved. Licensed under the University of Illinois/NCSA Open Source License. See LICENSE.txt in the project root for license information.
+
 using ClangSharp.Interop;
 
 namespace ClangSharp
 {
-    // e.g. `using namespace std;`
-    // See: https://clang.llvm.org/doxygen/classclang_1_1UsingDirectiveDecl.html
-    public class UsingDirectiveDecl: NamedDecl, IMergeable<UsingDecl>
+    public class UsingDirectiveDecl : NamedDecl
     {
         internal UsingDirectiveDecl(CXCursor handle) : base(handle, CXCursorKind.CXCursor_UsingDirective)
         {
