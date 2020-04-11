220e5e4120a00c851f0f0f10cb4866d4dea2306c
Tanner Gooding <tagoo@outlook.com>
2020-04-11 14:07:05 -0700

    Fixing up the managed API

--- sources/ClangSharp/Cursors/Exprs/CharacterLiteral.cs
+++ sources/ClangSharp/Cursors/Exprs/CharacterLiteral.cs
@@ -13,7 +13,7 @@ namespace ClangSharp
         internal CharacterLiteral(CXCursor handle) : base(handle, CXCursorKind.CXCursor_CharacterLiteral, CX_StmtClass.CX_StmtClass_CharacterLiteral)
         {
             _value = new Lazy<string>(() => {
-                var tokens = Handle.TranslationUnit.Tokenize(Extent);
+                var tokens = Handle.TranslationUnit.Tokenize(Handle.RawExtent);
 
                 Debug.Assert(tokens.Length == 1);
                 Debug.Assert(tokens[0].Kind == CXTokenKind.CXToken_Literal);
