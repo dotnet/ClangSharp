8e2c0c6b92d9464875e781cb97b9ff6f1f0d1a61
Tanner Gooding <tagoo@outlook.com>
2021-03-17 14:39:55 -0700

    Fixing build scripts to use pwsh and to call GenerateMetadataSource.ps1, rather than .cmd

--- pack.cmd
+++ pack.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0scripts\build.ps1""" -pack %*"
+pwsh.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0scripts\build.ps1""" -pack %*"
 EXIT /B %ERRORLEVEL%
