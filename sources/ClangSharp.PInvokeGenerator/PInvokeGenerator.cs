a285344699c9883b73e857820889efd3a1c6fcd6
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-08-11 18:12:05 -0700

    Update version check

--- sources/ClangSharp.PInvokeGenerator/PInvokeGenerator.cs
+++ sources/ClangSharp.PInvokeGenerator/PInvokeGenerator.cs
@@ -21,8 +21,8 @@ namespace ClangSharp
         private static readonly Encoding s_defaultStreamWriterEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
         private static readonly Regex s_needsSystemSupportRegex = new Regex(@"\b(?:Guid|IntPtr|UIntPtr)\b", RegexOptions.Compiled);
 
-        private const string ExpectedClangVersion = "14.0.";
-        private const string ExpectedClangSharpVersion = "14.0.";
+        private const string ExpectedClangVersion = "version 14.0";
+        private const string ExpectedClangSharpVersion = "version 14.0.";
 
         private readonly CXIndex _index;
         private readonly OutputBuilderFactory _outputBuilderFactory;
