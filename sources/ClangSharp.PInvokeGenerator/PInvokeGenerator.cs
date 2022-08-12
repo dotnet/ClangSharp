01fb41306ecbdabaa4feabcb1dec690c83b5ee41
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-08-11 17:30:24 -0700

    Add option to skip Clang and ClangSharp version checks, along with version patch leniency

--- sources/ClangSharp.PInvokeGenerator/PInvokeGenerator.cs
+++ sources/ClangSharp.PInvokeGenerator/PInvokeGenerator.cs
@@ -21,8 +21,8 @@ namespace ClangSharp
         private static readonly Encoding s_defaultStreamWriterEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
         private static readonly Regex s_needsSystemSupportRegex = new Regex(@"\b(?:Guid|IntPtr|UIntPtr)\b", RegexOptions.Compiled);
 
-        private const string ExpectedClangVersion = "14.0.0";
-        private const string ExpectedClangSharpVersion = "14.0.0";
+        private const string ExpectedClangVersion = "14.0.";
+        private const string ExpectedClangSharpVersion = "14.0.";
 
         private readonly CXIndex _index;
         private readonly OutputBuilderFactory _outputBuilderFactory;
@@ -64,7 +64,7 @@ namespace ClangSharp
         private CXXRecordDecl _cxxRecordDeclContext;
         private bool _disposed;
 
-        public PInvokeGenerator(PInvokeGeneratorConfiguration config, Func<string, Stream> outputStreamFactory = null)
+        public PInvokeGenerator(PInvokeGeneratorConfiguration config, Func<string, Stream> outputStreamFactory = null, bool ignoreClangVersion = false, bool ignoreClangSharpVersion = false)
         {
             if (config is null)
             {
@@ -73,14 +73,14 @@ namespace ClangSharp
 
             var clangVersion = clang.getClangVersion().ToString();
 
-            if (!clangVersion.Contains(ExpectedClangVersion))
+            if (!ignoreClangVersion && !clangVersion.StartsWith(ExpectedClangVersion))
             {
                 throw new InvalidOperationException($"Invalid libClang version. Returned string '{clangVersion}' does not contain '{ExpectedClangVersion}'");
             }
 
             var clangSharpVersion = clangsharp.getVersion().ToString();
 
-            if (!clangSharpVersion.Contains(ExpectedClangSharpVersion))
+            if (!ignoreClangSharpVersion && !clangSharpVersion.StartsWith(ExpectedClangSharpVersion))
             {
                 throw new InvalidOperationException($"Invalid libClang version. Returned string '{clangSharpVersion}' does not contain '{ExpectedClangSharpVersion}'");
             }
