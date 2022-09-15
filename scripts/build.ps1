889b3c4e75eb154e799e54b4b372a02baf4a6051
Tanner Gooding <tagoo@outlook.com>
2022-09-14 19:53:04 -0700

    Update to support .NET 7 and other basic infrastructure updates

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -173,6 +173,7 @@ try {
     Create-Directory -Path $DotNetInstallDirectory
 
     & $DotNetInstallScript -Channel 6.0 -Version latest -InstallDir $DotNetInstallDirectory -Architecture $architecture
+    & $DotNetInstallScript -Channel 7.0 -Version latest -InstallDir $DotNetInstallDirectory -Architecture $architecture -Quality preview
 
     $env:PATH="$DotNetInstallDirectory;$env:PATH"
   }
