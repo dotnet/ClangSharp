5c7c16cc9bcae62d9b1c50b33e2868b65b6027ce
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2024-05-28 22:07:02 +0200

    Add XML Serialization classes

---
+++ sources/ClangSharp.PInvokeGenerator/XML/ValueDescValueElement.cs
@@ -0,0 +1,21 @@
+﻿// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+using System.Xml.Serialization;
+
+namespace ClangSharp.XML;
+
+[XmlType]
+public class ValueDescValueElement
+{
+    [XmlElement("unchecked")]
+    public ValueDescValueElement? Unchecked { get; init; }
+
+    [XmlElement("cast")]
+    public string? CastTargetTypeName { get; init; }
+
+    [XmlElement("value")]
+    public ValueDescValueElement? NestedValue { get; init; }
+
+    [XmlElement("code")]
+    public CSharpCodeElement? Code { get; init; }
+}
