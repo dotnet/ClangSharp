9c786a995fc08a2214f15beffabce4f80b0a77c6
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-12-30 18:27:07 +0100

    Fix C# formatting.

--- tests/ClangSharp.UnitTests/ObjectiveCTest.cs
+++ tests/ClangSharp.UnitTests/ObjectiveCTest.cs
@@ -45,34 +45,35 @@ public sealed class ObjectiveCTest : TranslationUnitTest
 
         using var translationUnit = CreateTranslationUnit(inputContents, "objective-c++");
 
-        var classes = translationUnit.TranslationUnitDecl.Decls.OfType<ObjCInterfaceDecl>().ToList ();
-        Assert.That (classes.Count, Is.GreaterThanOrEqualTo (1), $"At least one class");
-        var myClass = classes.SingleOrDefault (v => v.Name == "MyClass")!;
-        Assert.That (myClass, Is.Not.Null, "MyClass");
+        var classes = translationUnit.TranslationUnitDecl.Decls.OfType<ObjCInterfaceDecl>().ToList();
+        Assert.That(classes.Count, Is.GreaterThanOrEqualTo(1), $"At least one class");
+        var myClass = classes.SingleOrDefault(v => v.Name == "MyClass")!;
+        Assert.That(myClass, Is.Not.Null, "MyClass");
 
-        var methodP1 = myClass.Methods.SingleOrDefault (v => v.Name == "P1")!;
-        Assert.That (methodP1, Is.Not.Null, "methodP1");
-        Assert.That (methodP1.IsPropertyAccessor, Is.True, "methodP1.IsPropertyAccessor");
+        var methodP1 = myClass.Methods.SingleOrDefault(v => v.Name == "P1")!;
+        Assert.That(methodP1, Is.Not.Null, "methodP1");
+        Assert.That(methodP1.IsPropertyAccessor, Is.True, "methodP1.IsPropertyAccessor");
 
-        var methodSetP1 = myClass.Methods.SingleOrDefault (v => v.Name == "setP1:")!;
-        Assert.That (methodSetP1, Is.Not.Null, "methodSetP1");
-        Assert.That (methodSetP1.IsPropertyAccessor, Is.True, "methodSetP1.IsPropertyAccessor");
+        var methodSetP1 = myClass.Methods.SingleOrDefault(v => v.Name == "setP1:")!;
+        Assert.That(methodSetP1, Is.Not.Null, "methodSetP1");
+        Assert.That(methodSetP1.IsPropertyAccessor, Is.True, "methodSetP1.IsPropertyAccessor");
 
-        var methodInstanceMethod = myClass.Methods.SingleOrDefault (v => v.Name == "instanceMethod")!;
-        Assert.That (methodInstanceMethod, Is.Not.Null, "methodInstanceMethod");
-        Assert.That (methodInstanceMethod.IsPropertyAccessor, Is.False, "methodInstanceMethod.IsPropertyAccessor");
+        var methodInstanceMethod = myClass.Methods.SingleOrDefault(v => v.Name == "instanceMethod")!;
+        Assert.That(methodInstanceMethod, Is.Not.Null, "methodInstanceMethod");
+        Assert.That(methodInstanceMethod.IsPropertyAccessor, Is.False, "methodInstanceMethod.IsPropertyAccessor");
 
-        var methodStaticMethod = myClass.Methods.SingleOrDefault (v => v.Name == "staticMethod")!;
-        Assert.That (methodStaticMethod, Is.Not.Null, "methodStaticMethod");
-        Assert.That (methodStaticMethod.IsPropertyAccessor, Is.False, "methodStaticMethod.IsPropertyAccessor");
+        var methodStaticMethod = myClass.Methods.SingleOrDefault(v => v.Name == "staticMethod")!;
+        Assert.That(methodStaticMethod, Is.Not.Null, "methodStaticMethod");
+        Assert.That(methodStaticMethod.IsPropertyAccessor, Is.False, "methodStaticMethod.IsPropertyAccessor");
     }
 
-    private static void AssertNeedNewClangSharp ()
+    private static void AssertNeedNewClangSharp()
     {
-        var forceRun = !string.IsNullOrEmpty (Environment.GetEnvironmentVariable ("FORCE_RUN"));
-        if (forceRun) {
+        var forceRun = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("FORCE_RUN"));
+        if (forceRun)
+        {
             return;
         }
-        Assert.Ignore ("TODO: this needs a new version of libClangSharp published.");
+        Assert.Ignore("TODO: this needs a new version of libClangSharp published.");
     }
 }
