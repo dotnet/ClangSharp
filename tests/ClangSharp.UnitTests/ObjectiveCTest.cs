64bcf6559b4fedc430c1808633bd952516f4e42e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-12-31 11:49:52 +0100

    Fix creating ObjCInterfaceType instances.

--- tests/ClangSharp.UnitTests/ObjectiveCTest.cs
+++ tests/ClangSharp.UnitTests/ObjectiveCTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation and Contributors. All Rights Reserved. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
 using System.Linq;
+using ClangSharp.Interop;
 using NUnit.Framework;
 
 namespace ClangSharp.UnitTests;
@@ -26,5 +27,34 @@ public sealed class ObjectiveCTest : TranslationUnitTest
         foreach (var c in categories) {
             Assert.That(c.TypeParamList.Count, Is.EqualTo (0), "TypeParamList.Count");
         }
+    }
+
+    [Test]
+    public void PointerTypes()
+    {
+        var inputContents = """
+@interface MyClass
+-(void)instanceMethod:(MyClass **)ptrToPtrToMyClass;
+@end
+""";
+        using var translationUnit = CreateTranslationUnit(inputContents, "objective-c++");
+
+        var classes = translationUnit.TranslationUnitDecl.Decls.OfType<ObjCInterfaceDecl>().ToList();
+
+        var myClass = classes.SingleOrDefault(v => v.Name == "MyClass")!;
+        Assert.That(myClass, Is.Not.Null, "MyClass");
+
+        var instanceMethod = myClass.Methods.SingleOrDefault(v => v.Name == "instanceMethod:")!;
+        Assert.That(instanceMethod, Is.Not.Null, "instanceMethod");
+        var parameters = instanceMethod.Parameters.ToList();
+        Assert.That(parameters.Count, Is.EqualTo(1), "parameters.Count");
+        var parameter = parameters[0];
+        Assert.That(parameter.Name, Is.EqualTo("ptrToPtrToMyClass"), "parameter.Name");
+        var parameterType = parameter.Type;
+        Assert.That(parameterType.Kind, Is.EqualTo(CXTypeKind.CXType_Pointer), "parameterType.Kind");
+        var pointeeType = parameterType.PointeeType;
+        Assert.That(pointeeType.Kind, Is.EqualTo(CXTypeKind.CXType_ObjCObjectPointer), "pointeeType.Kind");
+        var pointee2Type = pointeeType.PointeeType;
+        Assert.That(pointee2Type.Kind, Is.EqualTo(CXTypeKind.CXType_ObjCInterface), "pointee2Type.Kind");
     }
 }
