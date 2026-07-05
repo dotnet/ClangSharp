070ada1d069653f77ffa3b815377640f9fdbbf56
William Chen <exanite78012@gmail.com>
2026-07-05 19:30:53 -0400

    Edit wording

--- sources/ClangSharp.PInvokeGenerator/PInvokeGenerator.cs
+++ sources/ClangSharp.PInvokeGenerator/PInvokeGenerator.cs
@@ -6788,7 +6788,7 @@ public sealed partial class PInvokeGenerator : IDisposable
                 }
             }
 
-            // Need to cast to output type if out of range of corresponding 32-bit integer type
+            // Cast to output type if out of range of corresponding 32-bit integer type
             if (IsPrevContextDecl<VarDecl>(out _, out _) && !IsStmtAsWritten<CastExpr>(stmt, out _, removeParens: true)
                 && ((targetTypeName is "nuint" or "UIntPtr" && stmt.Handle.Evaluate.AsUnsigned > uint.MaxValue)
                     || (targetTypeName is "nint" or "IntPtr" && stmt.Handle.Evaluate.AsLongLong is < int.MinValue or > int.MaxValue)))
