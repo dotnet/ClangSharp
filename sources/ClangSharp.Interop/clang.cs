4d66b06094e263854f820f0d827ef295f9ed3dd5
Hadrian Tang <hadrianwttang@outlook.com>
2026-07-12 02:41:19 +0800

    Resolve review comments, second round

--- sources/ClangSharp.Interop/clang.cs
+++ sources/ClangSharp.Interop/clang.cs
@@ -15,6 +15,9 @@ public static unsafe partial class @clang
 {
     public static event DllImportResolver? ResolveLibrary;
 
+    public const int MajorVersion = 21;
+    public const int MinorVersion = 1;
+
     static @clang()
     {
         if (!Configuration.DisableResolveLibraryHook)
@@ -25,62 +28,137 @@ public static unsafe partial class @clang
 
     private static IntPtr OnDllImport(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
     {
+        // Let user hooks attempt to resolve first, so they can override the behavior for their app
         if (TryResolveLibrary(libraryName, assembly, searchPath, out var nativeLibrary))
         {
             return nativeLibrary;
         }
 
-        // On Unix, LoadLibraryHelper ignores the DllImportSearchPath flags, so we
-        // manually handle SafeDirectories by trying the application directory first
-        // (which SafeDirectories normally includes on Windows but is ignored on Unix),
-        // then fall back to the standard system search with the various names the native
-        // library can bind to.
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        // Then try the normal resolution of the library
+        if (TryResolve(libraryName, assembly, searchPath.GetValueOrDefault(), out nativeLibrary))
         {
-            if (searchPath.GetValueOrDefault().HasFlag(DllImportSearchPath.SafeDirectories))
-            {
-                // Try the names using AppContext.BaseDirectory
-                if (TryResolveClang(libraryName, AppContext.BaseDirectory, out nativeLibrary))
-                {
-                    return nativeLibrary;
-                }
-            }
+            return nativeLibrary;
+        }
 
-            // Try the standard variations via system search
-            if (TryResolveClang(libraryName, null, out nativeLibrary))
+        // Finally, do library specific resolution as applicable
+        if (libraryName.Equals("libclang", StringComparison.Ordinal))
+        {
+            if (TryResolveClang(assembly, searchPath, out nativeLibrary))
             {
                 return nativeLibrary;
             }
         }
 
-        return IntPtr.Zero;
+        return default;
     }
 
-    private static bool TryResolveClang(string libraryName, string? baseDirectory, out IntPtr nativeLibrary)
+    private static bool TryResolve(string libraryName, Assembly assembly, DllImportSearchPath searchPath, out IntPtr nativeLibrary)
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        if (TryResolveFromAppDirectory(libraryName, assembly, searchPath, out nativeLibrary))
         {
-            var path = (baseDirectory is not null ? Path.Combine(baseDirectory, libraryName) : libraryName) + ".dylib";
-            return NativeLibrary.TryLoad(path, out nativeLibrary);
+            return true;
         }
 
+        return NativeLibrary.TryLoad(libraryName, assembly, searchPath, out nativeLibrary);
+    }
+
+    private static bool TryResolveClang(Assembly assembly, DllImportSearchPath? searchPath, out IntPtr nativeLibrary)
+    {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
         {
-            if (baseDirectory is not null)
+            return TryResolve($"libclang.so.{MajorVersion}", assembly, searchPath.GetValueOrDefault(), out nativeLibrary)
+                || TryResolve($"libclang-{MajorVersion}", assembly, searchPath.GetValueOrDefault(), out nativeLibrary)
+                || TryResolve("libclang.so.1", assembly, searchPath.GetValueOrDefault(), out nativeLibrary);
+        }
+
+        nativeLibrary = default;
+        return false;
+    }
+
+    private static bool TryResolveFromAppDirectory(string libraryName, Assembly assembly, DllImportSearchPath searchPath, out IntPtr nativeLibrary)
+    {
+        nativeLibrary = default;
+
+        // DllImportSearchPath is ignored on non-Windows and the default resolution
+        // algorithm does not look in the application directory.
+        // We need to look here for any dependencies that are shipped SxS with the
+        // application, using the standard variations allowed.
+        if (OperatingSystem.IsWindows())
+        {
+            return false;
+        }
+
+        if (((searchPath & (DllImportSearchPath.SafeDirectories | DllImportSearchPath.ApplicationDirectory)) == 0))
+        {
+            return false;
+        }
+
+        if (Path.IsPathFullyQualified(libraryName))
+        {
+            return false;
+        }
+
+        // We use Environment.ProcessPath because that is the location of the executable
+        // that launched the process. AppContext.BaseDirectory can be overridden and is
+        // rather more the "AssemblyDirectory". This is a subtle distinction, but an important one.
+        var applicationDirectory = Path.GetDirectoryName(Environment.ProcessPath);
+
+        if (applicationDirectory is null)
+        {
+            return false;
+        }
+
+        var libraryPath = Path.Combine(applicationDirectory, libraryName);
+
+        if (NativeLibrary.TryLoad(libraryPath, assembly, searchPath, out nativeLibrary))
+        {
+            return true;
+        }
+
+        // We try the given name first, as that is always preferred. Then we try, in order:
+        //   * libname
+        //   * name.ext
+        //   * libname.ext
+        //
+        // This can result in some redundant lookups, but it's how the built-in logic works
+        // for non-relative paths.
+        var prefixedLibraryName = "";
+
+        if (!libraryName.Contains(Path.DirectorySeparatorChar, StringComparison.Ordinal))
+        {
+            prefixedLibraryName = $"lib{libraryName}";
+            libraryPath = Path.Combine(applicationDirectory, prefixedLibraryName);
+
+            if (NativeLibrary.TryLoad(libraryPath, assembly, searchPath, out nativeLibrary))
             {
-                return NativeLibrary.TryLoad(Path.Combine(baseDirectory, libraryName + ".so.20"), out nativeLibrary)
-                    || NativeLibrary.TryLoad(Path.Combine(baseDirectory, libraryName + "-20"), out nativeLibrary)
-                    || NativeLibrary.TryLoad(Path.Combine(baseDirectory, libraryName + ".so.1"), out nativeLibrary);
-            }
-            else
-            {
-                return NativeLibrary.TryLoad(libraryName + ".so.20", out nativeLibrary)
-                    || NativeLibrary.TryLoad(libraryName + "-20", out nativeLibrary)
-                    || NativeLibrary.TryLoad(libraryName + ".so.1", out nativeLibrary);
+                return true;
             }
         }
 
-        nativeLibrary = IntPtr.Zero;
+        var extension = ".so";
+
+        if (OperatingSystem.IsMacOS() || OperatingSystem.IsIOS() || OperatingSystem.IsTvOS())
+        {
+            extension = ".dylib";
+        }
+
+        libraryPath = Path.Combine(applicationDirectory, $"{libraryName}{extension}");
+
+        if (NativeLibrary.TryLoad(libraryPath, assembly, searchPath, out nativeLibrary))
+        {
+            return true;
+        }
+
+        if (prefixedLibraryName.Length != 0)
+        {
+            libraryPath = Path.Combine(applicationDirectory, $"{prefixedLibraryName}{extension}");
+
+            if (NativeLibrary.TryLoad(libraryPath, assembly, searchPath, out nativeLibrary))
+            {
+                return true;
+            }
+        }
+
         return false;
     }
 
